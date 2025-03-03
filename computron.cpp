a3989263a1ea1cc4913065eb52eae268e7a66cf9
lydianewhouse <lydianewhouse@vt.edu>
2025-03-03 17:27:27 -0500

    Set default opcode to command to halt

--- computron.cpp
+++ computron.cpp
@@ -61,7 +61,7 @@ Command opCodeToCommand(size_t opCode){
         case 41: return Command::branchNeg;
         case 42: return Command::branchZero;
         case 43: return Command::halt;
-        default: throw std::runtime_error("invalid_input (opCodeToCommand)");
+        default: return Command::halt;
     };
 }
 
